2c48ca23cd9ff8af794d403070f9c1ffea8ed734
helionext <juniorjobs.zen@gmail.com>
2022-04-10 19:51:13 -0300

    Algotimo 21

---
+++ alg21.js
@@ -0,0 +1,8 @@
+const rd = require('readline-sync');
+
+let num1 = rd.questionInt("Digite um número: ");
+let num2 = rd.questionInt("Digite outro número: ");
+
+const produto = num1 * num2;
+
+console.log(`\nProduto: ${produto}`);
