4b0d0e305d1e7f8144149f44df22bba881d7de5b
helionext <juniorjobs.zen@gmail.com>
2022-04-12 07:06:01 -0300

    Algoritimo 34

---
+++ alg34.js
@@ -0,0 +1,12 @@
+const rd = require('readline-sync');
+
+let base = rd.questionFloat("Base: ")
+let altura = rd.questionFloat("Altura: ")
+
+let area = base * altura
+let perimetro = 2 * (base + altura)
+let diagonal = Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2))
+
+console.log(`\nArea: ${area}`)
+console.log(`Perimetro: ${perimetro}`)
+console.log(`Diagonal: ${diagonal}`)
