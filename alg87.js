cd8e60838eab06d126595c71f8ca7628d3ba39ba
helionext <juniorjobs.zen@gmail.com>
2022-05-31 21:45:46 -0300

    Alg87

---
+++ alg87.js
@@ -0,0 +1,24 @@
+/**
+ * Entrar com um número e informar se ele é divisivel por 10, por 5, por 2 ou se
+ * não é divisivel por nenhum destes.
+ */
+
+const rd = require('readline-sync')
+
+let numero = rd.questionInt("Entre com um numero")
+
+console.log()
+
+if(numero % 2 === 0 && numero % 5 === 0 && numero % 10 === 0) {
+    if(numero % 2 === 0) {
+        console.log("Divisivel por 2.")
+    }
+    if(numero % 5 === 0) {
+        console.log("Divisivel por 5.")
+    }
+    if(numero % 10 === 0) {
+        console.log("Divisivel por 10.")
+    }
+} else {
+    console.log("Não é divisivel por 2, 5 e 10.")
+}
