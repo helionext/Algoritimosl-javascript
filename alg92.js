58812bd7d1bfa7021314894b47c805c3a9c5e819
helionext <juniorjobs.zen@gmail.com>
2022-07-03 17:05:26 -0300

    algoritimo 92

--- alg92.js
+++ alg92.js
@@ -1,3 +1,15 @@
 /**
- * 
+ * Entrar com um nume e imprimir uma das mensagens: maior do que 20, igual a 20 ou menor do que 20.
  */
+
+const rd = require('readline-sync');
+
+let numero = rd.questionInt("Entre com numero: ");
+
+if(numero > 20) {
+    console.log("Maior do que 20.");
+} else if(numero < 20) {
+    console.log("Menor do que 20.")
+} else {
+    console.log("Igual a 20.");
+}
