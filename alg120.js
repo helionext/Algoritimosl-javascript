329b6b043fda1632f36242a211548c0f9220a6bf
helionext <juniorjobs.zen@gmail.com>
2022-08-07 08:13:14 -0300

    alg120

---
+++ alg120.js
@@ -0,0 +1,9 @@
+/**
+ * Criar um algoritimo que imprima uma tabela de conversão de polegadas para centimetros
+ * . Deseja-se que na tabela conste valores desde 1 polegadas até 20 polegadas inteiras.
+ */
+
+
+for (let pol = 1; pol <= 20; pol++) {
+    console.log(`${pol} polegadas equivale a ${pol * 2.54} centimetros`);   
+}
