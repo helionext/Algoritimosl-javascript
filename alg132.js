dba97f2f6a954423e0edf9cc4d729ad26ef69949
helionext <juniorjobs.zen@gmail.com>
2022-08-09 21:59:23 -0300

    alg132

---
+++ alg132.js
@@ -0,0 +1,22 @@
+/**
+ * Criar um algoritimo que leia os limites inferior e superior de um intervalo
+ * e o número cujos múltiplos se deseja que sejam impressos no intervalo aberto.
+ */
+
+const rd = require('readline-sync');
+
+let inferior = rd.questionInt("Inferior: ");
+let superior = rd.questionInt("Superior: ");
+let multiplo = rd.questionInt("Numero multiplo: ");
+
+inferior++;
+
+if(inferior < superior) {
+    for (;inferior < superior; inferior++) {
+        if(inferior % multiplo === 0) {
+            console.log(inferior);
+        }
+    }
+} else {
+    console.log("Inferior deve ser maior do que superior: ");
+}
