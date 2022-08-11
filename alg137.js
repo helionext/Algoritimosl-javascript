fe64dcaf04bddc4ac9f907f5ffa0b6c5c0d7f905
helionext <juniorjobs.zen@gmail.com>
2022-08-11 20:47:12 -0300

    alg137

---
+++ alg137.js
@@ -0,0 +1,20 @@
+/**
+ * Criar um algoritimo que leia um número da entrada, leia n números inteiros de entrada e imprima
+ * o maior deles. Suponha que todos os numeros lidos sejam positivos.
+ */
+
+const rd = require('readline-sync');
+
+let num = rd.questionInt("Com quantos numeros deseja entrar: ");
+
+let maior = Number.NEGATIVE_INFINITY;
+
+for (let i = 0; i < num; i++) {
+    let valor = rd.questionInt("Entre com um valor: ");
+
+    if(valor > maior) {
+        maior = valor;
+    }
+}
+
+console.log(`\nMaior: ${maior}`);
