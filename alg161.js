862f6a1cbb6db63cdbd344daadefa7de05854bf7
helionext <juniorjobs.zen@gmail.com>
2022-08-21 16:55:25 -0300

    alg161

---
+++ alg161.js
@@ -0,0 +1,12 @@
+/**
+ * Imprimir todas as tabelas de multiplicar de 1 até 10.
+ */
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log();
+}
+
+
